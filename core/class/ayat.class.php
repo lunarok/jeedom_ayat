b27bd9f7d73b1a1ffb4b22f48815cb6fb098d885
Cédric Dupont <lunarok@gmail.com>
2017-04-16 18:07:55 +0200

    list witout modif

--- core/class/ayat.class.php
+++ core/class/ayat.class.php
@@ -138,7 +138,6 @@ class ayat extends eqLogic {
 			mkdir(dirname(__FILE__) . '/../../data');
 		}
         log::add('ayat', 'debug', 'list : ' . $list);
-        $list = substr_replace('[','',substr_replace(']','',substr_replace('"','',substr_replace(',','|',substr_replace('\\','',$list)))));
         $data_path = realpath(dirname(__FILE__)) . '/../../data/' . $this->getId() . '_' . $lang . '.mp3';
         $resource_path = realpath(dirname(__FILE__) . '/../../resources');
         log::add('ayat', 'debug', $resource_path . '/install.sh "' . $list . '" ' . $data_path );
